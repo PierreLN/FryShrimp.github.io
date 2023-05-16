28ffecffd9da4a92a6d25d5dc24b383d38f7e02e
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-05-15 20:15:06 -0400

    change

--- app/src/components/Homepage.tsx
+++ app/src/components/Homepage.tsx
@@ -7,16 +7,20 @@ const Homepage = (props: any) => {
       <div className={styles["homepage-bottom"]}>
         <div className={styles.title}>Welcome to SimpleLifeBalancing!</div>
         <div className={styles.description}>
-          <br />
+          <br/>
           Balancing your life is one of the most important aspect of your life
           and it’s critical to your dream’s success. Nevertheless, many of us
           neglect it. Fear not, it’s never too late to start taking control over
           your life. This blog will help you leverage yourself into a position
-          where you have more control over your life. “Invest in you.” Feel free
-          to navigate through the blog and look for the answers you need.
+          where you have more control over your life. 
+          <br/><br/>
+          “Invest in you.” 
+          <br/><br/>
+          Feel free to navigate through the blog and look for the answers you need.
+          <br/><br/>
           “Absorb what is useful. Reject what is useless. Add what is
           essentially your own.” – Bruce lee.
-          <br /> <br /> “Everything that I offer the world comes as a product of me
+          <br/><br/> “Everything that I offer the world comes as a product of me
           trying to help myself.” -Elliot Hulse
         </div>
       </div>
